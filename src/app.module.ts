00ae573974f21549c5fcd4867ddc4126f371ad44
NaoyaAbe <abechanfront@gmail.com>
2022-11-17 10:48:24 +0900

    SetUp prisma module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TodoModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TodoModule, PrismaModule],
 })
 export class AppModule {}
